46c46f12022f54f0ffec753025a366cb77d9ffb4
Kent Williams <k3nt00@gmail.com>
2025-02-03 22:04:28 -0800

    set text encoder regardless of previous set

--- src/utils/polyfill.js
+++ src/utils/polyfill.js
@@ -6,40 +6,18 @@ import TextDecoder from './TextDecoder'
 
 global.Buffer = global.Buffer || buffer.Buffer
 
-// Force-load encodings
+// Force load encodings
 iconv.encodings = encodings
 
-console.log(
-  'Checking global.TextDecoder properties:',
-  Object.getOwnPropertyDescriptor(global, 'TextDecoder'),
-)
-console.log('TextDecoder constructor:', global.TextDecoder)
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
 
-if (typeof global.TextDecoder !== 'undefined') {
-  try {
-    delete global.TextDecoder // Try to remove it if it's configurable
-    console.log('Deleted existing TextDecoder successfully.')
-  } catch (error) {
-    console.warn('Failed to delete existing TextDecoder:', error)
-  }
-}
+// Define global.TextDecoder (always do this)
+Object.defineProperty(global, 'TextDecoder', {
+  value: global.TextDecoder,
+  writable: false,
+  configurable: false,
+  enumerable: true,
+})
 
-// Now attempt to override it only if it was successfully deleted or not defined at all
-if (typeof global.TextDecoder === 'undefined') {
-  global.TextEncoder = TextEncoder
-  global.TextDecoder = TextDecoder
-
-  // Prevent future overwrites
-  Object.defineProperty(global, 'TextDecoder', {
-    value: global.TextDecoder,
-    writable: false,
-    configurable: false,
-    enumerable: true,
-  })
-
-  console.log('Polyfilled TextDecoder successfully applied.')
-} else {
-  console.warn(
-    'global.TextDecoder is already defined and non-configurable, skipping polyfill.',
-  )
-}
+console.log('Polyfill applied.')
